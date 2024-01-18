6a5211fdb96552987b75c20c2a7f25843a1085fb
Uthsara K A R <134998461+ravin00@users.noreply.github.com>
2024-01-19 04:09:11 +0530

    changes made

--- E-Shop/frontend/src/App.js
+++ E-Shop/frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import "./App.css";
 import {BrowserRouter,Routes,Route} from "react-router-dom" 
-import {LoginPage} from "./Routes.js";
+import {LoginPage,SignupPage} from "./Routes.js";
 
  const App = () => {
   return (
    <BrowserRouter>
    <Routes>
     <Route path = '/login' element= {<LoginPage/>} />
+    <Route path = '/sign-up' element= {<SignupPage/>} />
    </Routes>
    </BrowserRouter>
   )
--- E-Shop/frontend/src/Routes.js
+++ E-Shop/frontend/src/Routes.js
@@ -1,5 +1,8 @@
-import LoginPage from "../src/pages/Login.jsx";
+import LoginPage from "../src/pages/Login";
+import SignupPage from "../src/pages/SignupPage";
 
 export {
     LoginPage,
+    SignupPage
+
 }
